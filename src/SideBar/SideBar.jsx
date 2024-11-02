d628b36e9121437620d438930869fe8d751b73cc
MohmmadJamal <m.abomalash@student.aaup.edu>
2024-11-02 20:48:01 +0200

    Side bar Done

--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-
+import ListItem from "../custom/ListItem";
+import { iconConfig } from "./icons";
+import sideBarStyle from "./sidebar.module.css";
+import { FaXTwitter } from "react-icons/fa6";
+import PersonalProfile from "../custom/personalProfile/PersonalProfile";
 const SideBar = () => {
   return (
-    <div>
-      <h1>SideBar</h1>
+    <div className={sideBarStyle.sidebar}>
+      <div >
+      <div>
+        <FaXTwitter className={sideBarStyle.logo} />
+      </div>
+        {iconConfig.map((item, index) => (
+          <ListItem
+            key={index}
+            icon={<item.icon />}
+            title={item.title}
+            onClick={() => {}}
+          />
+        ))}
+        <button className={sideBarStyle.btn}>Post</button>
+      </div>
+      <PersonalProfile />
     </div>
   );
-}
+};
 
 export default SideBar;
---
+++ src/SideBar/icons.js
@@ -0,0 +1,21 @@
+import { GoHome } from "react-icons/go";
+import { IoSearchSharp } from "react-icons/io5";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa6";
+import { CiCircleMore } from "react-icons/ci";
+import { MdListAlt } from "react-icons/md";
+import { FaRegBookmark } from "react-icons/fa";
+import { HiOutlineUsers } from "react-icons/hi";
+
+export const iconConfig = [
+  { icon: GoHome, title: "Home" },
+  { icon: IoSearchSharp, title: "Explore" },
+  { icon: IoIosNotificationsOutline, title: "Notifications" },
+  { icon: MdOutlineEmail, title: "Messages" },
+  { icon: MdListAlt, title: "Lists" },
+  { icon: FaRegBookmark, title: "Bookmarks" },
+  { icon: HiOutlineUsers, title: "Communities" },
+  { icon: FaRegUser, title: "Profile" },
+  { icon: CiCircleMore, title: "More" },
+];
